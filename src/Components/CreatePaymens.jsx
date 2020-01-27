3b8ba9f960c71e3726a8eafa378bc4f2160eb39b
ildarnarbek <58545811+ildarnarbek@users.noreply.github.com>
2020-01-27 04:55:28 +0400

    Added form in component CreatePaymen
    Добавлена форма в  компонент создание платежа.
    Added form in Createpayment Component

---
+++ src/Components/CreatePaymens.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class CreatePayments extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <div>
+            <label htmlFor="paymentDescription">Наименование:</label>
+            <input
+              type="text"
+              name="paymentDescription"
+              id="paymentDescription"
+            />
+          </div>
+          <div>
+            <label htmlFor="paymentComment">Комментарий:</label>
+            <input type="text" name="paymentComment" id="paymentComment" />
+          </div>
+          <div>
+            <label htmlFor="paymentDate">Дата платежа:</label>
+            <input type="text" name="paymentDate" id="paymentDate" />
+          </div>
+          <div>
+            <label htmlFor="paymentSum">Суммв платежа:</label>
+            <input type="text" name="paymentSum" id="paymentSum" />
+          </div>
+          <div>
+            <label htmlFor="paymentReceiver">Реквизиты платежа:</label>
+            <input type="text" name="paymentReceiver" id="paymentReceiver" />
+          </div>
+
+          <button type="submit">Создать платеж</button>
+          <button type="submit">Отмена</button>
+        </form>
+      </div>
+    );
+  }
+}
