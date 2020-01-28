134314bf32457d31e45084e5bd77fdeaba71eb6b
ildarnarbek <58545811+ildarnarbek@users.noreply.github.com>
2020-01-28 23:56:30 +0400

    Add aythListener

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 import "./App.css";
 import AuthContainer from "./Components/AuthContainer";
+import Home from "./Components/Home";
 import { createStore } from "redux";
 import rootReduser from "./Store/reducers";
 import { Provider } from "react-redux";
+import fireb from "./configforfirebase/Fire";
 
 const store = createStore(rootReduser);
 
 // console.log(store.getState());
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fireb.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(this.state);
+        this.setState({ user });
+        localStorage.setItem("user", user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem("user");
+      }
+    });
+  }
+
   render() {
     return (
       <Provider store={store}>
-        <AuthContainer />
+        {this.state.user ? <Home /> : <AuthContainer />}
       </Provider>
     );
   }
