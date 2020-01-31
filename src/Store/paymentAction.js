682eec338416834fa734457c7279b331c5c4963c
ildarnarbek <58545811+ildarnarbek@users.noreply.github.com>
2020-02-01 00:03:42 +0400

    return old code for actions create payment

--- src/Store/paymentAction.js
+++ src/Store/paymentAction.js
@@ -1,45 +1,45 @@
 //позже положить в папку createpayments/actions
 import firebase from "../configforfirebase/Fire";
-// export const createPayment = payment => {
-//   return dispatch => {
-//     firebase
-//       .firestore()
-//       .collection("payments")
-//       .add({
-//         ...payment,
-//         paymentDate: new Date(),
-//         paymentStatus: "В процессе"
-//       })
-//       .then(() => {
-//         dispatch({ type: "CREATE_PAYMENT_SUCCESS", payment });
-//       })
-//       .catch(err => {
-//         dispatch({ type: "CREATE_PAYMENT_ERROR" }, err);
-//       });
-//   };
-// };
-
-export function createPayment(paymentObject) {
-  return function(dispatch) {
-    console.log("ADD_FIRESTORE_POST ACTION");
-
+export const createPayment = payment => {
+  return dispatch => {
     firebase
       .firestore()
       .collection("payments")
-      .add(paymentObject)
-      .then(payment => {
-        dispatch({
-          type: "CREATE_PAYMENT_SUCCESS",
-          payload: {
-            payment,
-            paymentDate: new Date(),
-            paymentStatus: "В процессе"
-          }
-        });
-        console.log("post has been added");
+      .add({
+        ...payment,
+        paymentDate: new Date(),
+        paymentStatus: "В процессе"
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PAYMENT_SUCCESS", payment });
       })
       .catch(err => {
-        console.log("post was NOT added");
+        dispatch({ type: "CREATE_PAYMENT_ERROR" }, err);
       });
   };
-}
+};
+
+// export function createPayment(paymentObject) {
+//   return function(dispatch) {
+//     console.log("ADD_FIRESTORE_POST ACTION");
+
+//     firebase
+//       .firestore()
+//       .collection("payments")
+//       .add(paymentObject)
+//       .then(payment => {
+//         dispatch({
+//           type: "CREATE_PAYMENT_SUCCESS",
+//           payload: {
+//             payment,
+//             paymentDate: new Date(),
+//             paymentStatus: "В процессе"
+//           }
+//         });
+//         console.log("post has been added");
+//       })
+//       .catch(err => {
+//         console.log("post was NOT added");
+//       });
+//   };
+// }
